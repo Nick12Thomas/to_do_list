265a5c64a12b2c02e1a62b2fc5428bc7fb9a7bd8
Nikhil S Thomas <102505264+Nick12Thomas@users.noreply.github.com>
2024-12-01 15:43:56 +0530

    fixed bugs

--- lib/models/task_data.dart
+++ lib/models/task_data.dart
@@ -1,7 +1,5 @@
 import 'dart:collection';
-
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
 import 'package:to_do_list/models/task.dart';
 
 class TaskData extends ChangeNotifier{
